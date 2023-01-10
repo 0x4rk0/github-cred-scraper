d5681cc8604fcf7b692d34b3ce4282c0164f8072
Cody Bernardy <43980696+0x4rk0@users.noreply.github.com>
2023-01-09 21:41:00 -0800

    Update cred_scrape.py

--- cred_scrape.py
+++ cred_scrape.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python
-
 import json
 import os
 import requests
 import sys
+
+organization = input("Enter the name of the organization: ")
 repo_urls = []
 
-for i in json.loads(requests.get('https://api.github.com/orgs/%s/repos' % sys.argv[1]).content.decode('utf-8')):
-    repo_urls.append(i['html_url'])
+response = requests.get('https://api.github.com/orgs/%s/repos' % organization)
+repos = json.loads(response.content.decode('utf-8'))
 
-for i in set(repo_urls):
-    print('[+] Hitting: %s' % i)
-    os.popen('trufflehog --regex --entropy=False %s >> %s_results.txt' %(i, sys.argv[1]))
+for repo in repos:
+    repo_urls.append(repo['html_url'])
+
+for url in set(repo_urls):
+    print('[+] Hitting: %s' % url)
+    os.popen('trufflehog --regex --entropy=False %s >> %s_results.txt' %(url, organization))
