97cb84b21ab436797774cf72ce48692619c7b94a
Cody Bernardy <43980696+0x4rk0@users.noreply.github.com>
2023-01-09 21:43:00 -0800

    Update cred_scrape.py

--- cred_scrape.py
+++ cred_scrape.py
@@ -6,8 +6,16 @@ import sys
 organization = input("Enter the name of the organization: ")
 repo_urls = []
 
-response = requests.get('https://api.github.com/orgs/%s/repos' % organization)
-repos = json.loads(response.content.decode('utf-8'))
+try:
+    response = requests.get('https://api.github.com/orgs/%s/repos' % organization)
+    response.raise_for_status()
+    repos = json.loads(response.content.decode('utf-8'))
+except requests.exceptions.HTTPError as e:
+    print("Error:", e)
+    sys.exit(1)
+except ValueError as e:
+    print("Error: Invalid JSON response.")
+    sys.exit(1)
 
 for repo in repos:
     repo_urls.append(repo['html_url'])
