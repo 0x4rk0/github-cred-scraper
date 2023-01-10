336db479a873e16afc88ee540a2cf597c6a36afa
Cody Bernardy <43980696+0x4rk0@users.noreply.github.com>
2023-01-09 21:37:08 -0800

    Converting to Python3

--- cred_scrape.py
+++ cred_scrape.py
@@ -6,12 +6,9 @@ import requests
 import sys
 repo_urls = []
 
-for i in json.loads(requests.get('https://api.github.com/orgs/%s/repos' % sys.argv[1]).content):
-
-  repo_urls.append(i['html_url'])
+for i in json.loads(requests.get('https://api.github.com/orgs/%s/repos' % sys.argv[1]).content.decode('utf-8')):
+    repo_urls.append(i['html_url'])
 
 for i in set(repo_urls):
-
-  print '[+] Hitting: %s' % i
-
-  os.popen('trufflehog --regex --entropy=False %s >> %s_results.txt' %(i, sys.argv[1]))
+    print('[+] Hitting: %s' % i)
+    os.popen('trufflehog --regex --entropy=False %s >> %s_results.txt' %(i, sys.argv[1]))
